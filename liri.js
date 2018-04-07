2db0bba18b803c17c8e37d0091394e804aede266
Paul Cheno <pwchenoweth@gmail.com>
2018-04-06 20:15:13 -0500

    if no args passed, do not error

--- liri.js
+++ liri.js
@@ -5,136 +5,139 @@ let keys = require("./keys")
 var request = require("request");
 // Store all of the arguments in an array
 var nodeArgs = process.argv;
-var action = nodeArgs[2].toLowerCase();
+// do we have the necessary inputs?
+if (nodeArgs.length > 2) {
+    var action = nodeArgs[2].toLowerCase();
 
-function movieThis(action, nodeArgs) {
-    // Create an empty variable for holding the movie name
-    var movieName = "";
-    // Add "+" for spaces in movie name
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        } else {
-            movieName += nodeArgs[i];
-        } //else
-    } //for 
-    if (movieName == "") {
-        movieName = "Mr.+Nobody"
-    }
-    // Then run a request to the OMDB API with the movie specified, using my apiKey
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=5f016458"
-    //* Title of the movie.
-    //* Year the movie came out.
-    //* IMDB Rating of the movie.
-    //* Rotten Tomatoes Rating of the movie.
-    //* Country where the movie was produced.
-    //* Language of the movie.
-    //* Plot of the movie.
-    //* Actors in the movie.
-    request(queryUrl, function (error, response, body) {
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-            var bodyData = JSON.parse(body);
-            if (bodyData.Response == "True") {
-                console.log("Title: " + bodyData.Title);
-                console.log("Release Year: " + bodyData.Year);
-                console.log("Title: " + bodyData.imdbRating);
-                var rottenTomatoes = "";
-                for (var i = 0; i < bodyData.Ratings.length; i++) {
-                    if (bodyData.Ratings[i].Source == "Rotten Tomatoes") {
-                        rottenTomatoes = bodyData.Ratings[i].Value;
-                    }
-                }
-                console.log("Rotten Tomatoes: " + rottenTomatoes);
-                console.log("Country: " + bodyData.Country);
-                console.log("Language: " + bodyData.Language);
-                console.log("Plot: " + bodyData.Plot);
-                console.log("Actors: " + bodyData.Actors);
+    function movieThis(action, nodeArgs) {
+        // Create an empty variable for holding the movie name
+        var movieName = "";
+        // Add "+" for spaces in movie name
+        for (var i = 3; i < nodeArgs.length; i++) {
+            if (i > 3 && i < nodeArgs.length) {
+                movieName = movieName + "+" + nodeArgs[i];
             } else {
-                console.log('Sorry, movie titled "' + movieName.split(/[+]+/).join(' ') + '" does not exist in the OMDB database.')
-            }
-        } //if (!error && response.statusCode === 200)
-    }); //request(queryUrl, function (error, response, body)
-} //function movieThis
-
-function twitterThis() {
-
-    var tw = require('twitter');
-    var client = new tw({
-        consumer_key: keys.twitter.consumer_key,
-        consumer_secret: keys.twitter.consumer_secret,
-        access_token_key: keys.twitter.access_token_key,
-        access_token_secret: keys.twitter.access_token_secret
-    });
-    var params = {
-        name: 'Keys Soze',
-        screen_name: 'KeysSoze',
-        exclude_replies: false,
-        trim_user: true,
-        count: 20
-    };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            for (var i = 0; i < tweets.length; i++) {
-                var n = i + 1;
-                console.log(n + '---------------------------------------------');
-                console.log('created:  ' + tweets[i].created_at);
-                console.log('text   :  ' + tweets[i].text);
-                console.log('-----------------------------------------------');
-            }
+                movieName += nodeArgs[i];
+            } //else
+        } //for 
+        if (movieName == "") {
+            movieName = "Mr.+Nobody"
         }
-    });
-
-} //function twitterThis
-
-if (action == "do-what-it-says") {
-    // Includes the FS package for reading and writing packages
-    var fs = require("fs");
-    // Running the readFile module that's inside of fs.
-    // Stores the read information into the variable "data"
-    fs.readFile("random.txt", "utf8", function (err, data) {
-        if (err) {
-            return console.log(err);
-        } else {
-            // Break the string down by comma separation and store the contents into the output array.
-            var output = data.split(",");
-            //initialize nodeArgs
-            var nodeArgs = [];
-            for (var i = 0; i < 3; i++) {
-                nodeArgs[i] = "";
-            }
-            var tempName = "";
-            // Loop Through the newly created output array
-            for (var i = 0; i < output.length; i++) {
-                if (i == 0) {
-                    action = output[i].toLowerCase();
-                } else {
-                    //take into consideration there maybe a "," in the movie title
-                    if (i == 1) {
-                        tempName = output[i];
-                    } else {
-                        tempName += "," + output[i];
+        // Then run a request to the OMDB API with the movie specified, using my apiKey
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=5f016458"
+        //* Title of the movie.
+        //* Year the movie came out.
+        //* IMDB Rating of the movie.
+        //* Rotten Tomatoes Rating of the movie.
+        //* Country where the movie was produced.
+        //* Language of the movie.
+        //* Plot of the movie.
+        //* Actors in the movie.
+        request(queryUrl, function (error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                var bodyData = JSON.parse(body);
+                if (bodyData.Response == "True") {
+                    console.log("Title: " + bodyData.Title);
+                    console.log("Release Year: " + bodyData.Year);
+                    console.log("Title: " + bodyData.imdbRating);
+                    var rottenTomatoes = "";
+                    for (var i = 0; i < bodyData.Ratings.length; i++) {
+                        if (bodyData.Ratings[i].Source == "Rotten Tomatoes") {
+                            rottenTomatoes = bodyData.Ratings[i].Value;
+                        }
                     }
+                    console.log("Rotten Tomatoes: " + rottenTomatoes);
+                    console.log("Country: " + bodyData.Country);
+                    console.log("Language: " + bodyData.Language);
+                    console.log("Plot: " + bodyData.Plot);
+                    console.log("Actors: " + bodyData.Actors);
+                } else {
+                    console.log('Sorry, movie titled "' + movieName.split(/[+]+/).join(' ') + '" does not exist in the OMDB database.')
                 }
-                // Print each element (item) of the array/
-            } // for (var i = 0; i < output.length; i++)
-            // now put movie name in nodeArgs as if user entered
-            var nameArray = tempName.split(" ");
-            for (var i = 0; i < nameArray.length; i++) {
-                nodeArgs.push(nameArray[i]);
-            }
+            } //if (!error && response.statusCode === 200)
+        }); //request(queryUrl, function (error, response, body)
+    } //function movieThis
 
-            if (action == "movie-this") {
-                movieThis(action, nodeArgs);
-            } else if (action == "my-tweets") {
-                twitterThis();
+    function twitterThis() {
+
+        var tw = require('twitter');
+        var client = new tw({
+            consumer_key: keys.twitter.consumer_key,
+            consumer_secret: keys.twitter.consumer_secret,
+            access_token_key: keys.twitter.access_token_key,
+            access_token_secret: keys.twitter.access_token_secret
+        });
+        var params = {
+            name: 'Keys Soze',
+            screen_name: 'KeysSoze',
+            exclude_replies: false,
+            trim_user: true,
+            count: 20
+        };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (!error) {
+                for (var i = 0; i < tweets.length; i++) {
+                    var n = i + 1;
+                    console.log(n + '---------------------------------------------');
+                    console.log('created:  ' + tweets[i].created_at);
+                    console.log('text   :  ' + tweets[i].text);
+                    console.log('-----------------------------------------------');
+                }
             }
-        } //else from err/readFile
-    }); // fs.readFile
-} else // if (action == "do-what-it-says") 
-    if (action == "movie-this") {
-        movieThis(action, nodeArgs);
-    } else if (action == "my-tweets") {
-    twitterThis();
-}
+        });
+
+    } //function twitterThis
+
+    if (action == "do-what-it-says") {
+        // Includes the FS package for reading and writing packages
+        var fs = require("fs");
+        // Running the readFile module that's inside of fs.
+        // Stores the read information into the variable "data"
+        fs.readFile("random.txt", "utf8", function (err, data) {
+            if (err) {
+                return console.log(err);
+            } else {
+                // Break the string down by comma separation and store the contents into the output array.
+                var output = data.split(",");
+                //initialize nodeArgs
+                var nodeArgs = [];
+                for (var i = 0; i < 3; i++) {
+                    nodeArgs[i] = "";
+                }
+                var tempName = "";
+                // Loop Through the newly created output array
+                for (var i = 0; i < output.length; i++) {
+                    if (i == 0) {
+                        action = output[i].toLowerCase();
+                    } else {
+                        //take into consideration there maybe a "," in the movie title
+                        if (i == 1) {
+                            tempName = output[i];
+                        } else {
+                            tempName += "," + output[i];
+                        }
+                    }
+                    // Print each element (item) of the array/
+                } // for (var i = 0; i < output.length; i++)
+                // now put movie name in nodeArgs as if user entered
+                var nameArray = tempName.split(" ");
+                for (var i = 0; i < nameArray.length; i++) {
+                    nodeArgs.push(nameArray[i]);
+                }
+
+                if (action == "movie-this") {
+                    movieThis(action, nodeArgs);
+                } else if (action == "my-tweets") {
+                    twitterThis();
+                }
+            } //else from err/readFile
+        }); // fs.readFile
+    } else // if (action == "do-what-it-says") 
+        if (action == "movie-this") {
+            movieThis(action, nodeArgs);
+        } else if (action == "my-tweets") {
+        twitterThis();
+    }
+}  //nodeArgs > 2
 //Paul Chenoweth week 11 node.js 04/06/2018
